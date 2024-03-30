60d6a1d17f236f5b40b355d3c41e8faf0650d05c
Sergey Aleshin <sergey.g.aleshin@gmail.com>
2024-03-30 22:52:21 +0300

    Typo fixed

--- Api/Messaging/VehicleStats.cs
+++ Api/Messaging/VehicleStats.cs
@@ -1,7 +1,7 @@
 ﻿namespace Api.Messaging {
     public class VehicleStats(double distanceInMeters, double distanceInMiles)
     {
-            public double Metres { get; set; } = distanceInMeters;
+            public double Meters { get; set; } = distanceInMeters;
 
             public double Miles { get; set; } = distanceInMiles;
     }
